856fbe303768d5b27133b2ad60bb8742be601192
WenTing <mybaby0103066@gmail.com>
2020-11-09 13:51:20 +0800

    完成登入檢查及會員身份導頁功能

--- check.php
+++ check.php
@@ -16,7 +16,30 @@ $pw=$_POST['pw'];
 $sql="select * from `login` where `acc`='$acc' && `pw`='$pw'";
 $check=$pdo->query($sql)->fetch();
 
-print_r($check);
+if(!empty($check)){
+  echo "登入成功";
+  $member_sql="select * from member where login_id='{$check['id']}'";
+  $member=$pdo->query($member_sql)->fetch();
+  $role=$member['role'];
+
+
+  switch($role){
+    case '會員';
+      header('location:mem.php');
+    break;
+    case 'VIP';
+    
+    header('location:vip.php');
+    break;
+    case '管理員';
+  
+    header('location:admin.php');
+    break;
+  }
+
+}else{
+  header("location:index.php?meg=帳密布正確，請重新登入或註冊新帳號");
+}
 
 
 ?>
