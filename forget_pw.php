b2e8c797383bdb67c10cd3525523a32c4c050453
WenTing <mybaby0103066@gmail.com>
2020-11-12 11:50:38 +0800

    新增忘記密碼功能

--- forget_pw.php
+++ forget_pw.php
@@ -6,6 +6,33 @@
     <title>忘記密碼</title>
 </head>
 <body>
-    
+<?php
+if(isset($_POST['email'])){
+    $dsn="mysql:host=localhost;dbname=member;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
+    $sql="select * from login where email='{$_POST['email']}'";
+    $chk=$pdo->query($sql)->fetch();
+    if(!empty($chk)){
+        $res=$chk['pw'];
+
+    }else{
+        $res="查無此人";
+    }
+}
+
+
+?>
+
+    <form action="?" method="post">
+        <input type="text" name="email">
+        <input type="submit" value="查詢">
+    </form>
+    <span>
+    <?php
+    if(isset($res)){
+        echo $res;
+    }
+    ?>
+    </span>
 </body>
 </html>
